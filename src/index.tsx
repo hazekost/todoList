01f04e483374580de465e6fdde3f221e75974f6c
hazekost <kostyk0007@gmail.com>
2021-09-19 18:11:32 +0300

    w1

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppWithRedux from './AppWithRedux';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppWithRedux />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
