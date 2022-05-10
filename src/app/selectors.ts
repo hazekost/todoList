dde3c8f7876852efd6cb3b2c6cf8225b510c3e5b
hazekost <kostyk0007@gmail.com>
2022-05-10 10:40:12 +0300

    add TodoList component

--- src/app/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RequestStatusType } from "./app-reducer"
-import { AppRootStateType } from "./store"
-
-export const selectStatus = (state: AppRootStateType): RequestStatusType => state.app.status
-export const selectIsInitialized = (state: AppRootStateType): boolean => state.app.isInitialized
